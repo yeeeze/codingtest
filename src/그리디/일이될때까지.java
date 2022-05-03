9f9b476aaebf8d10c50b7a7de7da1214db53a947
yeezze <86194303+yeezze@users.noreply.github.com>
2022-05-04 00:07:45 +0900

    Refactor: 1이 될 떄까지 오류 수정

--- src/그리디/일이될때까지.java
+++ src/그리디/일이될때까지.java
@@ -5,6 +5,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/**
+ * n < k인 경우에는 n/k가 안되니까.. 나눌 수 없기 때문에 그냥 1씩 빼줘야함
+ * n >= k, n < k 케이스를 나눠서 처리해주어야하는데 그걸 생각 못 했음..
+ */
+
 public class 일이될때까지 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,15 +19,22 @@ public class 일이될때까지 {
         int k = Integer.parseInt(st.nextToken());
         int cnt = 0;
 
-        if(n % k != 0) {
-            cnt = n % k;
-            n = n - (n % k);
+        while (n != 1) {
+            if(n >= k) {
+                if(n % k != 0) {
+                    cnt += n % k;
+                    n = n - (n % k);
+                }
+                n = n / k;
+                cnt++;
+            }
+            else {
+                n = n - 1;
+                cnt++;
+            }
         }
 
-        while (n != 1) {
-            n = n / k;
-            cnt++;
-        }
+
 
         System.out.println(cnt);
     }
