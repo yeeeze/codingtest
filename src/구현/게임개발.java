5e8f617accdbd410fed7f82099de0e32cdf396b3
yeezze <86194303+yeezze@users.noreply.github.com>
2022-05-19 12:40:51 +0900

    게임 개발
    캐릭터 시작 위치부터 cnt+=1, 가본칸으로 변경

--- src/구현/게임개발.java
+++ src/구현/게임개발.java
@@ -32,39 +32,40 @@ public class 게임개발 {
         // 캐릭터 이동 방향 (북, 동, 남, 서)
         int[] dx = {-1, 0, 1, 0};
         int[] dy = {0, 1, 0, -1};
-        int cnt = 0;
+        int cnt = 1;
         int turn = 0;
+        map[currentRow][currentColumn] = 2; // 가본 칸은 2로 바꿈
 
         while (true) {
-            int row = 0;
-            int column = 0;
-
             // 왼쪽으로 회전
             direction = turnLeft(direction);
             turn++;
 
-            row = currentRow + dx[direction];
-            column = currentColumn + dy[direction];
+            int row = currentRow + dx[direction];
+            int column = currentColumn + dy[direction];
 
             // 가보지 않은 칸이 존재한다면 왼쪽 한칸 전진
             if(map[row][column] == 0) {
                 currentRow = row;
                 currentColumn = column;
 
-                cnt++;
+                cnt += 1;
                 turn = 0;
                 map[row][column] = 2;   // 가본 칸은 2로 바꿈
             }
             else {
-                // 네 방향 모두 이비 가본 칸이거나 바다로 되어 있는 경우 뒤로 후진
+                // 네 방향 모두 이미 가본 칸이거나 바다로 되어 있는 경우 뒤로 후진
                 if(turn == 4) {
                     // 뒤쪽 방향이 바다이면 움직임을 멈춘다
-                    if(map[currentRow - dx[direction]][currentRow - dy[direction]] ==  1) {
+                    row = currentRow - dx[direction];
+                    column = currentRow - dy[direction];
+                    if(map[row][column] ==  1) {
                         break;
                     }
                     else {
-                        currentRow = currentRow - dx[direction];
-                        currentRow = currentRow - dy[direction];
+                        currentRow = row;
+                        currentColumn = column;
+                        turn = 0;
                     }
                 }
             }
@@ -76,7 +77,7 @@ public class 게임개발 {
     // 왼쪽으로 회전 함수
     public static int turnLeft(int direction) {
         int result = 0;
-        result = direction-1;
+        result = direction - 1;
 
         if(result == -1) {
             return 3;
