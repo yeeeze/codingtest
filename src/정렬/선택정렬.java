b353ebfbc34125a27ab487516f200a7eb746a454
yeezze <86194303+yeezze@users.noreply.github.com>
2022-05-23 19:02:03 +0900

    선택 정렬

---
+++ src/정렬/선택정렬.java
@@ -0,0 +1,25 @@
+package 정렬;
+
+public class 선택정렬 {
+
+    public static void main(String[] args) {
+        int[] arr = {7, 5, 9, 0, 3, 1, 6, 2, 4, 8};
+        int min;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if(arr[min] > arr[j]) {
+                    min = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+
+        for (int ar : arr) {
+            System.out.println(ar);
+        }
+    }
+}
