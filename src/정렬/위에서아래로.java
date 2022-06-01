6f595864be7670a0d73ea90968442fc5380dad06
yeezze <86194303+yeezze@users.noreply.github.com>
2022-06-01 23:40:58 +0900

    6장 정렬: 위에서 아래로

---
+++ src/정렬/위에서아래로.java
@@ -0,0 +1,31 @@
+package 정렬;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class 위에서아래로 {
+
+    public static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        Integer[] arr = new Integer[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        // 내림차순 정렬
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        for (int a : arr) {
+            System.out.print(a + " ");
+        }
+    }
+
+}
