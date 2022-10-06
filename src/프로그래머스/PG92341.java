a30b60c9f5e7eab10ed0b1cd32ac3f54d7bfe909
yeezze <86194303+yeezze@users.noreply.github.com>
2022-10-06 12:41:15 +0900

    PG92341

--- src/프로그래머스/PG92341.java
+++ src/프로그래머스/PG92341.java
@@ -8,15 +8,18 @@ import java.util.StringTokenizer;
 /**
  * 반례를 못 찾겠다 ㅠㅠㅠㅠㅠㅠㅠㅠㅠ^ㅠㅠㅠㅠㅠㅠㅠㅠㅠ...
  * 56.3 / 100
+ *
+ * 토탈 시간 계산하는 방식이 틀렸었음. 시간을 처음부터 모두 '분'단위로 환산하여 저장했더니 맞았음. (시 * 60)
+ * 올림처리할 때 int, double 처리 주의 (0.067 -> 1 이 되야되는데 int로 하면 0으로 들어가서 0이 리턴됨)
  */
 public class PG92341 {
 
     public static void main(String[] args) {
-//        int[] fees = {120, 0, 60, 591};
-//        String[] records = {"16:00 3961 IN","16:00 0202 IN","18:00 3961 OUT","18:00 0202 OUT","23:58 3961 IN"};
-
-        int[] fees = {60, 10, 59, 1};
-        String[] records = {"21:59 1234 IN"};
+        int[] fees = {120, 0, 60, 591};
+        String[] records = {"16:00 3961 IN","16:00 0202 IN","18:00 3961 OUT","18:00 0202 OUT","23:58 3961 IN"};
+//
+//        int[] fees = {60, 10, 59, 1};
+//        String[] records = {"21:59 1234 IN"};
 
         int a = 1 / 60;
         int b = (int) Math.ceil(a);         // 0
@@ -37,7 +40,11 @@ public class PG92341 {
         // 1. 차량별 누적 주차 시간을 구한다
         for (int i = 0; i < records.length; i++) {
             StringTokenizer st = new StringTokenizer(records[i]);
+
             String time = st.nextToken();
+            String t[] = time.split(":");
+            int nowTime = Integer.parseInt(t[0]) * 60 + Integer.parseInt(t[1]);
+
             String id = st.nextToken();
             String state = st.nextToken();
 
@@ -51,22 +58,9 @@ public class PG92341 {
                     car.time = time;
                 } else {
                     String in[] = car.time.split(":");
-                    String out[] = time.split(":");
+                    int inTime = Integer.parseInt(in[0]) * 60 + Integer.parseInt(in[1]);
 
-                    int hour = Integer.parseInt(out[0]) - Integer.parseInt(in[0]);
-                    int mini = 0;
-
-                    int outMini = Integer.parseInt(out[1]);
-                    int inMini = Integer.parseInt(in[1]);
-                    // out - in
-                    if (outMini >= inMini) {
-                        mini = outMini - inMini;
-                    } else {
-                        mini = 60 - inMini;
-                        hour -= 1;
-                    }
-                    mini += hour * 60;  // 시간 -> 분 환산
-                    car.totaltime += mini;
+                    car.totaltime += nowTime - inTime;
                 }
                 car.state = state;
             }
@@ -77,21 +71,9 @@ public class PG92341 {
             Car car = hashMap.get(key);
             if (car.state.equals("IN")) {
                 String in[] = car.time.split(":");
+                int inTime = Integer.parseInt(in[0]) * 60 + Integer.parseInt(in[1]);
 
-                int hour = 23 - Integer.parseInt(in[0]);
-                int mini = 0;
-
-                int outMini = 59;
-                int inMini = Integer.parseInt(in[1]);
-                // out - in
-                if (outMini >= inMini) {
-                    mini = outMini - inMini;
-                } else {
-                    mini = 60 - inMini;
-                    hour -= 1;
-                }
-                mini += hour * 60;  // 시간 -> 분 환산
-                car.totaltime += mini;
+                car.totaltime += 1439 - inTime;     // 23:59 -> 1439분
             }
         }
 
