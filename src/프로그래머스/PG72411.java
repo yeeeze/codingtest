3f16cbfa9d88e68e84e29bfb533cb2fadd86ef61
yeezze <yeeeze22@gmail.com>
2022-11-04 14:05:16 +0900

    refactor: PG 72411

--- src/프로그래머스/PG72411.java
+++ src/프로그래머스/PG72411.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 
 /**
  * 1. course 반복문 안에서 주문마다 발생할 수 있는 조합을 뽑는다. course의 갯수 == 조합 갯수 (알파벳 2개부터 가능)
- * 2. 가장 많이 나온 조합을 정답으로 추가한다.
+ * 2. 가장 많이 나온 조합을 정답으로 추가한다. -> max 값을 조합 메소드 내에서 추가할 때! 체크한다
  */
 
 public class PG72411 {
@@ -21,13 +21,14 @@ public class PG72411 {
     }
 
     private static HashMap<String, Integer> map = new HashMap();
+    private static int max;
 
     public static String[] solution(String[] orders, int[] course) {
         String[] answer = {};
 
         ArrayList<String> list = new ArrayList<>();
 
-        // 정렬
+        // 1. orders 문자열 개별 정렬
         for (int i = 0; i < orders.length; i++) {
             char[] chars = orders[i].toCharArray();
             Arrays.sort(chars);
@@ -35,24 +36,13 @@ public class PG72411 {
         }
 
         for (int i = 0; i < course.length; i++) {
+            max = 0;
             for (int j = 0; j < orders.length; j++) {
                 peek(orders[j], 0, course[i], 0, "");
             }
 
-            int max = Integer.MIN_VALUE;
-            String maxKey = "";
             for (String key : map.keySet()) {
-                if (max < map.get(key) && map.get(key) > 1) {
-                    max = map.get(key);
-                    maxKey = key;
-                }
-            }
-            if (maxKey != "") {
-                list.add(maxKey);
-            }
-
-            for (String key : map.keySet()) {
-                if (!list.contains(key) && max == map.get(key) && map.get(key) > 1) {
+                if (max == map.get(key) && map.get(key) > 1) {
                     list.add(key);
                 }
             }
@@ -69,6 +59,7 @@ public class PG72411 {
     public static void peek(String menu, int cnt, int size, int index, String peekMenu) {
         if (cnt == size) {
             map.put(peekMenu, map.getOrDefault(peekMenu, 0) + 1);
+            max = Math.max(max, map.get(peekMenu));
             return;
         }
 
